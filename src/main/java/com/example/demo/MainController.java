779a3e11e7231bcb9694dc6fdf91a25ea11bc3ac
konstantinGulish <konstantin_gulish@yahoo.com>
2018-06-26 10:19:39 -0400

    added search function on navbar

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -6,54 +6,65 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.List;
 
-    @Controller
+@Controller
     public class MainController {
-        @Autowired
-        PersonRepository people;
+    @Autowired
+    PersonRepository people;
 
-        @RequestMapping("/")
-        public String displayHome() {
-            return "index";
-        }
-
-
-        @GetMapping("/addperson")
-        public String addPerson(Model model) {
-            model.addAttribute("person", new Person());
-            return "addperson";
-        }
-
-        @PostMapping("/process")
-        public String processForm(@Valid Person person, BindingResult result) {
-            if (result.hasErrors()) {
-                return "addperson";
-            }
-            people.save(person);
-            return "redirect:/showpeople";
-        }
-
-        @RequestMapping("/showpeople")
-        public String showPeople(Model model) {
-            model.addAttribute("people", people.findAll());
-            return "listpeople";
-        }
-
-        @RequestMapping("/detail/{id}")
-        public String showJob(@PathVariable("id") long id, Model model) {
-            model.addAttribute("person", people.findById(id).get());
-            return "show";
-        }
-        @RequestMapping("/update/{id}")
-        public String updatePerson ( @PathVariable("id") long id, Model model){
-            model.addAttribute("person", people.findById(id).get());
-            return "addperson";
-        }
-
-        @RequestMapping("/delete/{id}")
-        public  String delCourse(@PathVariable("id") long id){
-            people.deleteById(id);
-            return "listpeople";
-        }
+    @RequestMapping("/")
+    public String displayHome() {
+        return "index";
     }
+
+
+    @GetMapping("/addperson")
+    public String addPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "addperson";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Person person, BindingResult result) {
+        if (result.hasErrors()) {
+            return "addperson";
+        }
+        people.save(person);
+        return "redirect:/showpeople";
+    }
+
+    @RequestMapping("/showpeople")
+    public String showPeople(Model model) {
+        model.addAttribute("people", people.findAll());
+        return "listpeople";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showJob(@PathVariable("id") long id, Model model) {
+        model.addAttribute("person", people.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updatePerson(@PathVariable("id") long id, Model model) {
+        model.addAttribute("person", people.findById(id).get());
+        return "addperson";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delCourse(@PathVariable("id") long id) {
+        people.deleteById(id);
+        return "listpeople";
+    }
+
+    @PostMapping("/showresults")
+    public String showResults(Model model, HttpServletRequest request)  {
+        String q = request.getParameter("query");
+        List<Person> results = people.findByFoodIgnoreCase(q);
+        model.addAttribute("results", results);
+        return "listpeople";
+    }
+}
--- src/main/java/com/example/demo/PersonRepository.java
+++ src/main/java/com/example/demo/PersonRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository <Person, Long> {
+
+    List<Person> findByFoodIgnoreCase(String food);
 }
